a51ad789470fe165c7a188ced93fdb1b30cf7bb3
Kaushal Prakash <kingkaushal1289@gmail.com>
2024-11-12 23:05:32 +0530

    added home animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
+'use client'
 import Navbar from "@/components/navbar/Navbar";
 import TypewriterText from "@/components/typewriter text/TypewriterText";
 import Image from "next/image";
+import gsap from "gsap";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [loading,setLoading] = useState(true);
+  useEffect(()=> {
+    const t1 = gsap.timeline();
+    t1.fromTo(".home-logo",
+      {scale:0 , opacity:0},
+      {scale:1, opacity:1, duration:1,delay:1}
+    ).fromTo('.home-diamond',
+      {y:20,opacity:0},
+      {opacity:1,y:0,duration:1,ease:"elastic.out"}
+    ).fromTo(".home-tagline",
+      {y:30,opacity:0},
+      {y:0,opacity:1,scale:1,duration:1,ease:"elsatic.out"}
+    )
+  },[loading])
+
   return (
     <div className="absolute inset-0 -z-10 min-h-screen w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
       <div className="relative z-10">
@@ -16,21 +34,21 @@ export default function Home() {
             alt="Astoneaone Logo"
             width={200}  // Set the appropriate width for the logo
             height={200} // Set the appropriate height for the logo
-            className="max-w-full h-auto" // Ensure it remains responsive
+            className="max-w-full h-auto home-logo" // Ensure it remains responsive
           />
         </div>
 
         {/* Diamond Group text */}
         <div className="text-center mb-4 translate-y-72 md:translate-y-48">
-          <p className="text-xl font-semibold text-purple-600">Diamond Group</p>
+          <p className="text-xl font-semibold text-purple-600 home-diamond">Diamond Group</p>
         </div>
 
         {/* Typewriter Text */}
-        <div className="text-center mb-8">
+        <div className="text-center mb-8 home-tagline">
           <TypewriterText s="Trusted Care, Every Day" />
         </div>
 
-        
+
       </div>
     </div>
   );
